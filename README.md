00d29dc716d4dfaa6eb8383dca51b47ca4d9da73
MrMike92 <sandovalpinedamiguelangel@gmail.com>
2025-01-06 08:21:51 -0600

    Actualización - Versión 3

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# Detección de placas vehiculares.
+# Detección de placas vehiculares
 Un detector de placas vehiculares la cual usa paralelismo para la reducción de tiempo.
 
-## Instrucciones de uso.
+# Índice
+
+* [Instrucciones de uso](#Instrucciones-de-uso)
+* [Herramientas utilizadas](#Tegnologías-Utilizadas)
+    * [Tesseract OCR](#Tesseract-OCR)
+    * [YOLO11](#YOLO11)
+    * [RoboFlow](#RoboFlow)
+    * [OpenCV](#OpenCV)
+* [Funcionamiento](#Funcionamiento)
+    * [Procesamiento de imagenes](#Procesamiento-de-imagenes)
+    * [Procesamiento de video](#Procesamiento de video)
+* [Nota](#Nota)
+
+# Instrucciones de uso
 
 - Clona este repositorio.
 - Abre el programa que deseas ejecutar en tu entorno de desarrollo que soporte Python:
@@ -13,15 +26,55 @@ Un detector de placas vehiculares la cual usa paralelismo para la reducción de 
 - Instalar las siguentes bibliotecas:
 ```python
 # Copiar y pegar lo siguente en el CMD de Windows.
-pip install opencv-python
 pip install numpy
 pip install pytesseract
 pip install supervision
 pip install ultralytics
+pip install opencv-python
 ```
 
-## Funcionamiento.
-### Procesamiento de imagenes
+# Herramientas utilizadas
+
+## Tesseract OCR
+
+Es un motor OCR de código abierto que extrae texto impreso o escrito de las imágenes. Fue desarrollado originalmente por Hewlett-Packard, y su desarrollo fue adquirido después por Google. Por eso se conoce ahora como “Google Tesseract OCR”. 
+
+### Instalación
+
+1. Descargar e instalar el ejecutable de [Tesseract OCR](https://github.com/tesseract-ocr/tessdoc?tab=readme-ov-file#binaries)
+2. Instalar la biblioteca pytesseract
+```python
+# Copiar y pegar lo siguente en el CMD de Windows.
+pip install pytesseract
+```
+
+> [!IMPORTANT]
+> Si previamente instalo las bibliotecas de la sección [Instrucciones de uso](#Instrucciones-de-uso), no es necesario volver a instalarlo.
+
+## YOLO11
+
+YOLO11 es la última iteración de la serie Ultralytics YOLO de detectores de objetos en tiempo real, que redefine lo que es posible con una precisión, velocidad y eficacia de vanguardia. Basándose en los impresionantes avances de las versiones anteriores de YOLO , YOLO11 introduce mejoras significativas en la arquitectura y los métodos de entrenamiento, lo que lo convierte en una opción versátil para una amplia gama de tareas de visión por ordenador.
+
+## RoboFlow
+
+Es una plataforma integral que proporciona herramientas para que los desarrolladores construyan, entrenen e implementen modelos de visión por computadora de manera eficiente.
+
+#### [Dataset utilizado de Roboflow Universe Projects](https://universe.roboflow.com/roboflow-universe-projects/license-plate-recognition-rxg4e)
+
+## OpenCV
+
+Es una biblioteca de software de aprendizaje automático y visión artificial de código abierto. OpenCV se creó para proporcionar una infraestructura común para aplicaciones de visión artificial y para acelerar el uso de la percepción artificial en productos comerciales. Al ser un producto con licencia Apache 2, OpenCV facilita a las empresas la utilización y modificación del código.
+
+```python
+# Copiar y pegar lo siguente en el CMD de Windows para instalar OpenCV
+pip install opencv-python
+```
+
+> [!IMPORTANT]
+> Si previamente instalo las bibliotecas de la sección [Instrucciones de uso](#Instrucciones-de-uso), no es necesario volver a instalarlo.
+
+# Funcionamiento
+## Procesamiento de imagenes
 
 <br> 1. Descargar la base de datos, descomprimir el archivo ZIP y quedarse solo con la carpeta **images**.
 
@@ -34,23 +87,24 @@ pip install ultralytics
 > [!WARNING]
 > Asegurese que la carpeta **images** y el archivo ***change_names.py*** esten en la misma carpeta.
 
-<br> 3. Ejecutar  ***recorta.py***.
+<br> 3. Ejecutar ***recorta.py***.
 
 > [!IMPORTANT]
 > Cambiar el valor de *num_threads* a un valor que este dentro del rango de la cantidad de procesadores lógicos de tu procesador
 
-### Procesamiento de video
+## Procesamiento de video
 > [!IMPORTANT]
 > Los videos utilizados para este proyecto pertenecen a sus resprectivos creadores.
-> <br><br>Link del video *test* y *test3*: https://www.youtube.com/watch?v=QmwIjn6rwQA
-> <br><br>Link del video *test2*: https://www.youtube.com/watch?v=-UQbT7ncCbs&t=55s
+> <br><br>Link del video *test* y *test2*: https://www.youtube.com/watch?v=QmwIjn6rwQA
 
-<br> 1. Descargar el repositorio o solo el contenido de la carpeta **procesar_videos** y descargar el video del test2, si es que se desea utilizar.
+<br> 1. Descargar el repositorio o solo el contenido de la carpeta **procesar_videos**.
 
 > [!WARNING]
 > Asegurese que se hayan descargado correctamente y que esten en la misma carpeta los videos que el archivo ***detector.py***.
 
 <br> 2. Ejecutar el archivo ***detector.py*** que se encuentra en la carpeta **procesar_videos**.
+
+# Nota
 
 Este proyecto se distribuye bajo la Licencia MIT. Consulta el archivo LICENSE para obtener más detalles.
 
--- procesar_videos/detector.py
+++ procesar_videos/detector.py
@@ -1,111 +1,99 @@
-# LP - License Plates               V - Vehicle
+# Codigo que realiza una sola vez el analisis del video sin aplicación de computo paralelo
+# p - Placas               t - Transporte
 
 import cv2
 import pytesseract
 import numpy as np
+# from time import time
 import supervision as sv
 from ultralytics import YOLO
 
-# Recortar imagenes
-def cropped(detections,image):
+# Función para recortar imágenes
+def cropped(detections, image):
     bounding_box = detections.xyxy
     xmin, ymin, xmax, ymax = bounding_box[0] # Extraer las coordenadas de la caja delimitadora
-    xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax) # Asegurar de que las coordenadas sean enteros
+    xmin, ymin, xmax, ymax = int(xmin), int(ymin), int(xmax), int(ymax)
     cropped_image = image[ymin:ymax, xmin:xmax] # Recortar la imagen usando las coordenadas de la caja delimitadora
+    
     return cropped_image
 
 def main():
-    # Parte 1: Procesar el video/dividirlo en frames
-    cap = cv2.VideoCapture('videos/test3.mp4') # Cambiar por el la ubicación del archivo a anilzar
+    # start_time = time()
+    cap = cv2.VideoCapture('videos/test.mp4')
     frame_number = 0
 
     if not cap.isOpened():
-       print("Error: No se puede abrir el archivo de video")
-       exit()
+        print("Error: No se pudo abrir el video")
+        exit()
 
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) # Ancho del frame
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # Largo del frame
     fps = cap.get(cv2.CAP_PROP_FPS)
     print(fps, "FPS")
-    output = 'videos/output_video_test3_preentrenadoV2.mp4'
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codificador para el archivo de salida
-    out = cv2.VideoWriter('videos/output_video_test3_preentrenadoV2.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+    output_video_path = 'videos/output_video_test.mp4'
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Codificador para el archivo de salida
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
+    model_t = YOLO('models\\yolo11n.pt') # Modelo para detectar vehículos
 
     while cap.isOpened():
-        ret,frame = cap.read() # Leer el video frame a frame
-        frame_number +=1 
+        ret, frame = cap.read() # Leer el video frame a frame
+        frame_number += 1 
 
         if not ret:
-           break 
+            break 
 
-        model_V = YOLO('models\\yolo11n.pt') # Modelo para detectar los vehiculos
-        print("Numero de frame: ",frame_number) # Número de frame que se está analizando
-        results_V = model_V(frame)[0] # Pasar el frame por el modelo que detecta los vehiculos
-        detection_V = sv.Detections.from_ultralytics(results_V) # Pasar los resultados a supervison
-        class_id = [2, 3, 5, 7] # Etiquetas: car, motorcycle, bus and truck (automovil, motocicleta, autobús y camión)
+        print("Número de frame: ", frame_number) # Número de frame analizado
+        results_t = model_t(frame)[0] # Pasar el frame por el modelo de YOLO
+        detections_t = sv.Detections.from_ultralytics(results_t) # Convertir resultados para Supervision
+        class_id = [2, 3, 5, 7] # Etiquetas: car, motorcycle, bus, truck
+        detections_t = detections_t[np.isin(detections_t.class_id, class_id)] # Filtrar detecciones solo para los vehículos
 
-        # Filtrar y mostrar solo las detecciones de 
-        if detection_V.class_id[0] in class_id:
-            bounding_box_annotator = sv.BoundingBoxAnnotator()
+        if len(detections_t) > 0:
+            # Descomentar si se desea ver el recuadro del vehiculo
+            """bounding_box_annotator = sv.BoundingBoxAnnotator()
             label_annotator = sv.LabelAnnotator()
-            annotated_image_V = bounding_box_annotator.annotate(scene=frame, detections=detection_V)
-            annotated_image_V = label_annotator.annotate(scene=annotated_image_V, detections=detection_V)            
-            cropped_image_V = cropped(detection_V, frame) # Recortar de imagen de vehiculo
-            model_LP = YOLO('models\\placa.pt') # Modelo para detectar el medio de matricula
-            results_LP = model_LP(cropped_image_V, agnostic_nms = True)[0] # Se pasa la imagen recortada por el modelo que detecta matriculas
-            results_LP.names[0] = "Matricula"
-            detections_LP = sv.Detections.from_ultralytics(results_LP) # Pasar los resultados a la libreria supervison
-            cropped_image_LP = cropped(detections_LP, cropped_image_V) # Obtener la imagen de la matricula antes de cambiar 
+            annotated_image_t = bounding_box_annotator.annotate(scene=frame, detections=detections_t)
+            annotated_image_t = label_annotator.annotate(scene=annotated_image_t, detections=detections_t)"""
+            cropped_image_t = cropped(detections_t, frame)
+            model_p = YOLO('models\\placa.pt') # Modelo para detectar matrículas
+            results_p = model_p(cropped_image_t, agnostic_nms=True)[0]
+            detections_p = sv.Detections.from_ultralytics(results_p)
 
-            # Parte 2: Pasar las coordenadas de la matricula a la imagen recortada del vehiculo
+            if len(detections_p) > 0:
+                cropped_image_matricula = cropped(detections_p, cropped_image_t)
 
-            # Diferencia de las coordenadas XY, el largo y ancho de la caja de la matricula
-            dif_x = results_LP.boxes.xyxy[0][2] - results_LP.boxes.xyxy[0][0]
-            dif_y = results_LP.boxes.xyxy[0][3] - results_LP.boxes.xyxy[0][1]
-            #Puntos iniciales, suma del punto de deteccion de la placa mas el del vehiculo
-            x1_nuevo = detection_V.xyxy[0][0] + detections_LP.xyxy[0][0]
-            y1_nuevo = detection_V.xyxy[0][1] + detections_LP.xyxy[0][1] 
-            # Sumar del punto inicial mas las dimensiones de la box de matricula.
-            x2_nuevo = x1_nuevo + dif_x  
-            y2_nuevo = y1_nuevo + dif_y 
-            # Guardar las nuevas coordenadas 
-            detections_LP.xyxy = np.array([[x1_nuevo,y1_nuevo,x2_nuevo,y2_nuevo]])
-            bounding_box_annotator = sv.BoundingBoxAnnotator()
-            label_annotator = sv.LabelAnnotator()
-            # Pasar la informacion que se mostrara en las etiqutes
-            annotated_image_LP = bounding_box_annotator.annotate(scene=frame, detections=detections_LP)
-            annotated_image_LP = label_annotator.annotate(scene=annotated_image_LP, detections=detections_LP)
+                # Lectura OCR con Tesseract
+                gray = cv2.cvtColor(cropped_image_matricula, cv2.COLOR_BGR2GRAY)
+                pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+                data = pytesseract.image_to_string(
+                    gray, lang='eng',
+                    config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+                )
+                data = data.strip() # Limpiar la cadena
+
+                # Añadir texto de la matrícula
+                text = data
+                position = (900, 60) # Posición del texto
+                font = cv2.FONT_HERSHEY_SIMPLEX # Tipo de fuente
+                font_scale = 2 # Tamaño/escala de la fuente
+                font_color = (255, 255, 255) # Color del texto, el formato es (Blue,Green,Red)
+                font_thickness = 6 # Grosor de la fuente
+                frame = cv2.putText(frame, text, position, font, font_scale, font_color, font_thickness) # Comentar si se desea ver el recuadro de la placa
+                # frame = cv2.putText(annotated_image_p, text, position, font, font_scale, font_color, font_thickness) # Descomentar si se desea ver el recuadro de la placa
+                # print(text) # Imprimir en consola el texto detectado en ese frame
+
+        out.write(frame)
+        # cv2.imshow('Frame', frame) # Mostrar el frame mientras se guarda
         
-            # Parte 3: Lectura de la placa con tesseract OCR
-
-            gray = cv2.cvtColor(cropped_image_LP, cv2.COLOR_BGR2GRAY) # Transformar a escala de grises
-            pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-
-            # Pasar el OCR por la imagen en escala de grises, que filtra solo numero y letras
-            data = pytesseract.image_to_string(gray, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnopqrstuvwxyz')
-
-            # Limpieza de la cadena de salida del OCR
-            valor_medio = round(len(data)/2)
-            data = data[valor_medio-3:valor_medio+4]
-            
-            # Parte 4: agregar la martricula en la imagen
-
-            text = data
-            position = (900, 60)  # Definir la posición del texto (se puede ajustar)
-            font = cv2.FONT_HERSHEY_SIMPLEX # Fuente del texto
-            font_scale = 2 # Tamaño del texto
-            font_color = (255, 255, 255)  # Color del texto en BGR
-            font_thickness = 6 # Grosor de las letras
-            frame = cv2.putText(annotated_image_LP, text, position, font, font_scale, font_color, font_thickness) # Añadir el texto a la imagen
-            print(text)
-            out.write(frame) # Guardar el frame para reconstruir el video
-            # cv2.imshow('Frame', frame) # Mostrar el frame mientra se guardan para el video
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-               break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
     out.release()
-   
+    cv2.destroyAllWindows()
+    """end_time = time()
+    execution_time = end_time - start_time
+    print(f"Tiempo de ejecución: {execution_time:.2f} segundos")"""
+    
 if __name__ == "__main__":
     main()
